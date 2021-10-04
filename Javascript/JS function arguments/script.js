0a37b98faaa470d01966af5f57a178f8fee07fb0
Linda Denissen <lintje82@gmail.com>
2021-10-04 15:25:07 +0200

    arrow

---
+++ Javascript/JS function arguments/script.js
@@ -0,0 +1,21 @@
+
+
+function colorsWall(Color) {
+  console.log('The wall has been painted red');
+  console.log('The wall has been painted ' + Color);
+  
+
+};
+
+colorsWall('green');
+
+
+const sideWall = function (color) {
+  console.log('The North wall has been painted ' + northwall);
+  console.log('The south-east wall has been painted ' + southwall);
+};
+
+let northwall = 'orange';
+let southwall = 'grey';
+
+sideWall();
