f872b817f4d0ebfd6fec0991346f22c77085cb70
Linda Denissen <lintje82@gmail.com>
2021-10-04 13:41:05 +0200

    arrow function

---
+++ Javascript/basic arrow/script.js
@@ -0,0 +1,28 @@
+//deel A
+const ikRockArrowFunctions = () => {
+  console.log ("Joe, ik rock de arrow functions!")
+};
+
+ikRockArrowFunctions () 
+
+
+//deel B 
+const fivePlusSeven = () => 5 + 7 
+
+
+ console.log (fivePlusSeven());
+
+//deel C
+let myFunction = (a, b) => a + b
+
+console.log (myFunction(2, 4));
+
+//deel D
+let addFive = a => a + 5;
+
+console.log (addFive (18));
+
+//deel E
+let createObject = () => ({greeting: "hoi"})
+
+console.log(createObject());
