b88f6edb880f03df6d7a0b33b2d2908b043c380b
Linda Denissen <lintje82@gmail.com>
2021-10-10 18:56:20 +0200

    array methods deel 2

--- Javascript/array methods deel 2/script.js
+++ Javascript/array methods deel 2/script.js
@@ -1,58 +1,81 @@
 const superheroes = [
-  {name: "Batman", alter_ego: "Bruce Wayne"}, 
-  {name: "Superman", alter_ego: "Clark Kent"}, 
-  {name: "Spiderman", alter_ego: "Peter Parker"}]
-  
-  // Hier komt jouw functie
-  var findSpiderMan = superheroes.find (function(element){
-    return element.name == "Spiderman";
-  })
+  { name: "Batman", alter_ego: "Bruce Wayne" },
+  { name: "Superman", alter_ego: "Clark Kent" },
+  { name: "Spiderman", alter_ego: "Peter Parker" }
+];
 
-    
-  console.log(findSpiderMan(superheroes)) 
+const findSpiderMan = superheroes.find(hero => hero.name === 'Spiderman');
+
+console.log(findSpiderMan);
+
   // Find Spiderman
   // result should be: {name: "Spiderman", alter_ego: "Peter Parker"}
 
-//B 
-let doubleArrayValues = ['1', '2', '3',];
-let newArr = doubleArrayValues.map (myFunction);
 
-function myFunction(num){
-  return num * 2;
+//B 
+
+const doubleArrayValues = function(array) {
+  let newArray = [];
+  array.forEach(number => {
+    newArray.push(number * 2);
+  });
+  return newArray;
 };
 
-console.log(num);
+console.log (doubleArrayValues([1, 2, 3]))
+// result should be [2, 4, 6]
+
 
 //C 
-const containsNumberBiggerThan10 = function (number){
-    number.some(number => number > 10);
-}
+const containsNumberBiggerThan10 = function(array) {
+  return array.some(number => {
+    return number > 10;
+  });
+};
+// Let op! Wanneer je van een functie verwacht dat deze True of False (een boolean) retourneert, is het gebruikelijk om er een werkwoord voor te zetten.
+// zodat het een gesloten vraag wordt.
+console.log("Je suis bigger than 10:",
+  containsNumberBiggerThan10([1, 4, 3, 6, 9, 7, 11])
+);
 
-containsNumberBiggerThan10([1, 4, 3, 6, 9, 7, 11]) 
-  // result should be true
-  containsNumberBiggerThan10([1,2,1,2,1,2])
-  // result should be false
 
 //D 
 const isItalyInTheGreat7 = function (land) {
-  let found = land.find (land => land.name === 'Italy');
+  return land.includes('Italy');
 };
 
 console.log (isItalyInTheGreat7(['Canada', 'France','Germany','Italy','Japan','United Kingdom','United States'])) 
 
-//E 
-const tenfold = function (num){
-  let result = (num * 10);
-}
 
+//E 
+const tenfold = function (array){
+  let newArray = [];
+  array.forEach(number => {
+    newArray.push(number * 10);
+  })
+  return newArray;
+}
 
 console.log(tenfold([1, 4, 3, 6, 9, 7, 11]))
 // result should be [10, 40, 30, 60, 90, 70, 110]
 
+
 //F 
-const isBelow100 = function (number){
-  let result = number.every (isBelow100 < 100);
+const isBelow100 = function (array) {
+  return array.every (number => number <= 100)
+};
+
+console.log('onder de 100? ' + isBelow100([1, 81, 4, 53, 3, 6, 79, 2, 43, 7, 28, 101, 11, 77, 84, 98 ]))
+// result should be: false
+
+console.log(
+  "Onder de 100, Should be true:",
+  isBelow100([1, 81, 4, 53, 3, 6, 79, 2, 43, 7, 28, 11, 77, 84, 98]))
+
+//G 
+const bigSum = function (items){
+return items.reduce((sum, item) => sum + item)
 }
 
-console.log(isBelow100([1, 81, 4, 53, 3, 6, 79, 2, 43, 7, 28, 101, 11, 77, 84, 98 ]))
-// result should be: false
+console.log(bigSum([1, 81, 4, 53, 3, 6, 79, 2, 43, 7, 28, 11, 77, 84, 98, 101, 206, 234]))
+// result should be 1118
